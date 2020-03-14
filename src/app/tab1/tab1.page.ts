65056d6e2758eaf31301a720ba2835a3071642ed
anabias <60902514+anabias@users.noreply.github.com>
2020-03-13 22:27:02 -0300

    Criação do serviço da API

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../servicos/user.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  //variaveis para paginação
+  public page = 1;
+  public totalPaginas = 0;
 
-  constructor() {}
+  //dados dos usuarios
+  public usuariosPagina = 0;
+  public totalUsuario=0;
+
+  //lista para usuarios
+  public listaUsuarios =[];
+
+  constructor(private userService: UserService ) {}
+
+  public buscarUsuarios(pagina:number){
+    this.userService.listarUsuarios(pagina).subscribe(dados=>{
+
+    });
+  }
 
 }
